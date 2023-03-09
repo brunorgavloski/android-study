b862840f906c020db0abdcc251a6e951c870f486
Bruno Gavloski <gavloski11@gmail.com>
2023-03-08 21:36:37 -0300

    edit text class

--- Widgets/app/src/main/java/com/example/widgets/MainActivity.java
+++ Widgets/app/src/main/java/com/example/widgets/MainActivity.java
@@ -3,17 +3,18 @@ package com.example.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    TextView textView;
+    EditText nomeBruno;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.textView3Bruno);
+        nomeBruno = findViewById(R.id.editTextBruno);
+        String input_text = nomeBruno.getText().toString();
     }
 }
