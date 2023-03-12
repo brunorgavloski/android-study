859538e3493bf4f76cb5df2ae7a8ed1ca8f38a33
Bruno Gavloski <gavloski11@gmail.com>
2023-03-11 21:42:09 -0300

    button

--- Widgets/app/src/main/java/com/example/widgets/MainActivity.java
+++ Widgets/app/src/main/java/com/example/widgets/MainActivity.java
@@ -3,18 +3,28 @@ package com.example.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText nomeBruno;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nomeBruno = findViewById(R.id.editTextBruno);
-        String input_text = nomeBruno.getText().toString();
+        button = findViewById(R.id.button101);
+
+        //handling the click events on button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //action to perform when button is clicked
+                Toast.makeText(MainActivity.this, "Button clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
