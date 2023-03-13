a5f1b7f761b0818889c0e054c21d4094d245243d
Bruno Gavloski <gavloski11@gmail.com>
2023-03-12 21:44:55 -0300

    image-view

--- Widgets/app/src/main/java/com/example/widgets/MainActivity.java
+++ Widgets/app/src/main/java/com/example/widgets/MainActivity.java
@@ -3,28 +3,21 @@ package com.example.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+    ImageView image;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button101);
+        image = findViewById(R.id.ImageView2);
+        image = findViewById(R.id.imageView22);
 
-        //handling the click events on button
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //action to perform when button is clicked
-                Toast.makeText(MainActivity.this, "Button clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
 
     }
 }
