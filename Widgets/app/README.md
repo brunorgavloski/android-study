eb20c37bc0ecb0f6c0ab6a28ae8e2d9f2030fdd2
Bruno Gavloski <gavloski11@gmail.com>
2023-03-14 21:14:46 -0300

    final do primeiro APP

---
+++ Widgets/app/README.md
@@ -0,0 +1,1 @@
+<h2>Primeiro aplicativo android</h2>
--- Widgets/app/src/main/java/com/example/widgets/MainActivity.java
+++ Widgets/app/src/main/java/com/example/widgets/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +27,14 @@ public class MainActivity extends AppCompatActivity {
         btn = findViewById(R.id.btn1);
         editText = findViewById(R.id.editText1);
 
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editText.getText().toString();
+
+                Toast.makeText(MainActivity.this, "Hello: " +name, Toast.LENGTH_LONG).show();
+            }
+        });
 
 
     }
